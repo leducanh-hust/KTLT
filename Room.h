fd331ed17ca73332b73c29cbc6a15729aa174576
Le Duc Anh <114907987+leducanh-hust@users.noreply.github.com>
2023-02-19 12:25:30 +0700

    Update Room.h

--- Room.h
+++ Room.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include <iostream>
 #include <string>
 
@@ -10,32 +10,43 @@ using namespace std;
 class Room
 {
 private:
-    int m_num; //Số phòng
-    int m_capacity; // Số người
-    bool m_reserved; // Biến logic kiểm tra phòng đã được đặt hay chưa ( 0 / 1)
-    double m_pricePerNight; // giá phòng 1 đêm 
+    int m_num;
+    int m_capacity;
+    bool m_reserved;
+    double m_pricePerNight;
 
 public:
-    //Constructor
-    Room();
+    Room() {}
     Room(int num, int capacity, bool reserved, double pricePerNight) : m_num(num), m_capacity(capacity), m_reserved(reserved), m_pricePerNight(pricePerNight) {}
 
-    //Get mothod
-    int getNum();
-    int getCapacity();
-    bool getIsReserved();
-    double getPricePerNight();
+    int getNum() { return this->m_num; }
+    int getCapacity() { return this->m_capacity; }
+    bool getIsReserved() { return this->m_reserved; }
+    double getPricePerNight() { return this->m_pricePerNight; }
+    void setNum(int m_num)
+    {
+        this->m_num = m_num;
+    }
+    void setCapacity(int m_capacity)
+    {
+        this->m_capacity = m_capacity;
+    }
+    void setIsReserved(bool m_reserved)
+    {
+        this->m_reserved = m_reserved;
+    }
+    void setPricePerNight(double m_pricePerNight)
+    {
+        this->m_pricePerNight = m_pricePerNight;
+    }
+    void reserve() { m_reserved = true; }
+    void unreserve() { m_reserved = false; }
 
-
-    //Set method
-    void setNum(int m_num);
-    void setCapacity(int m_capacity);
-    void setIsReserved(bool m_reserved);
-    void setPricePerNight(double m_pricePerNight);
-    void reserve();
-    void unreserve();
-
-    friend ostream& operator<<(ostream& os, const Room& room);
+    friend ostream &operator<<(ostream &os, const Room &room)
+    {
+        os << "Room " << room.m_num << " - capacity: " << room.m_capacity << ", Price for 1 night: " << room.m_pricePerNight << ", reserved: " << (room.m_reserved ? "yes" : "no");
+        return os;
+    }
     virtual void printRoomsid()
     {
         cout << "Room " << getNum() << endl;
@@ -46,4 +57,4 @@ public:
     friend class Hotel;
 };
 
-#endif 
+#endif
